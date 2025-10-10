ac59a1d2fb5b3f08c174e564c2222254f2af3e20
Anna Zhevlakova <azevlakova12@gmail.com>
2025-10-10 21:53:41 +0400

    fixed after review

--- src/main/java/com/example/DAO/Hibernate/entity/Person.java
+++ src/main/java/com/example/DAO/Hibernate/entity/Person.java
@@ -5,11 +5,8 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Builder;
-import lombok.Data;
-
 
 @Builder
-@Data
 @Entity
 @Table(name = "persons", schema = "netology")
 public class Person {
--- src/main/java/com/example/DAO/Hibernate/entity/PersonId.java
+++ src/main/java/com/example/DAO/Hibernate/entity/PersonId.java
@@ -3,13 +3,11 @@ package com.example.DAO.Hibernate.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PersonId implements Serializable {
